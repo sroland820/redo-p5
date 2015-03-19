08953ba74b156e37365b342b389e0766974c0aea
Sammie <sroland820@eagles.ewu.edu>
2015-03-19 00:39:52 -0700

    v0

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+// click button, capture zip field
+$('#getZip').click(function() {
+  
+  // stop default, <form> from submitting data
+  event.preventDefault();
+  
+  // get and store data
+  var zip = $('#zip').val();
+  console.log(zip);  
+  
+  // Get Weather
+  $.simpleWeather({
+    
+    location: zip,  
+    success: function(weather) {
+      
+      // get weather OK? 
+      console.log(weather);
+
+      $('.temp').text(weather.temp);
+      $('.city').text(weather.city);
+      $('.country').text(weather.country);
+    
+    },  
+    error: function(error) {
+      $('body').html('<p>' + error + '</p>');
+    }
+  
+  });
+  
+  // clear zip
+  $('#getWeather').val('');
+  
+});
+
+// DO NOT ERASE ABOVE THIS//
